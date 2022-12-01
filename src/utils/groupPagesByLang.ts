1f84c9f8842ba8cb34b38eb1c15e817e417952cb
Vladimir Lukyanov <vladimir@liikyanov.com>
2022-12-01 17:24:10 +0300

    v0.1.6 – package.json update – slug update

--- src/utils/groupPagesByLang.ts
+++ src/utils/groupPagesByLang.ts
@@ -2,7 +2,6 @@ import type { MarkdownInstance } from 'astro';
 
 export const groupPagesByLang = <T extends MarkdownInstance<Record<string, unknown>>>(pages: T[]) =>
 	pages.reduce((pages, page) => {
-		console.log(page.url);
 		const lang = page.url.split('/')[1];
 		if (!pages[lang]) pages[lang] = [];
 		pages[lang].push(page);
