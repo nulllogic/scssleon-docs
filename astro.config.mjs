9774e4d193a760e2738db9b6d388402455c05900
Vladimir Lukyanov <vladimir@liikyanov.com>
2023-05-22 10:17:51 +0300

    v0.4.62 – docs highlighter added – snippet component update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import {defineConfig} from 'astro/config';
+
+import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), preact()],
     output: 'server',
     markdown: {
         extendDefaultPlugins: true,
