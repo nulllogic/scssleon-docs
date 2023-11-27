7a246ab96e7cbf3dd912855c97120e11674de88c
Vladimir Lukyanov <vladimir@lukyanov.net>
2023-11-27 08:43:29 -1000

    v0.4.674
    - added github link
    - added examples schema
    - 404 updated

--- src/schemas/examples.ts
+++ src/schemas/examples.ts
@@ -1,6 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { z } from 'astro:content';
-export const docsSchema = z.object({
+export const examplesSchema = z.object({
   title: z.string(),
   description: z.string().optional(),
   group: z.string().optional(),
