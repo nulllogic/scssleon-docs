4d918a31290b37b3fa4c245ce11952906dd5a7ef
Vladimir Lukyanov <vladimir@lukyanov.net>
2023-11-28 03:58:44 -1000

    v0.4.676
    - updated examples layout
    - update config / collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,20 @@
 // 1. Import utilities from `astro:content`
-import {defineCollection} from 'astro:content';
-import {docsSchema} from "../schemas";
+import { defineCollection } from 'astro:content';
+import { docsSchema, examplesSchema } from "../schemas";
 
 // 2. Define a `type` and `schema` for each collection
 const docsCollection = defineCollection({
-    type: 'content',
-    schema: docsSchema,
+  type: 'content',
+  schema: docsSchema,
 });
+
+const examplesCollection = defineCollection({
+  type: 'content',
+  schema: examplesSchema
+})
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-    'docs': docsCollection,
+  'docs': docsCollection,
+  'examples': examplesCollection
 };
