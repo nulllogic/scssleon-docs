8989da12609b25a8499a2697ccb48829fe6c128c
vladimirlukyanov <vladimir@lukyanov.net>
2024-04-30 11:11:44 +0300

    v0.4.952 - components update - config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,23 @@ import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const tt = "test";
+
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), preact()],
+    compressHTML: true,
     output: 'server',
     markdown: {
         extendDefaultPlugins: true,
+    },
+    vite : {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `$url : ${tt};`,
+                },
+            },
+        }
     }
 });
