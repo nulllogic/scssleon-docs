beb90be7991313dd3a4204d0893a8c176f5bac71
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-07-03 21:22:15 +0300

    v0.5.725 – content update – sections update

---
+++ src/content/content.config.mjs
@@ -0,0 +1,45 @@
+import {defineCollection, reference, z} from 'astro:content';
+
+import {glob} from 'astro/loaders';
+
+const docs = defineCollection({
+  loader: glob({base: './src/content/docs', pattern: '**/*.*'}),
+  schema: ({image}) => z.object({
+    title: z.string(),
+    description: z.string().optional(),
+
+    published_date: z.date().optional(),
+    updated_date: z.date().optional(),
+  }),
+});
+
+const categories = defineCollection({
+  loader: glob({base: './src/content/categories', pattern: '**/*.*'}),
+  schema: z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+  })
+});
+
+const sections = defineCollection({
+  loader: glob({base: './src/content/sections', pattern: '**/*.*'}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    categories: z.array(reference('categories')).optional(),
+
+    image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
+  })
+});
+
+export const collections = {docs, sections, categories};
