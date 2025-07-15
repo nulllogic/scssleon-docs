dc35d1447a9cdfffc349a36929b4a338a2ee6f30
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-07-16 00:36:20 +0300

    v0.5.784 – snippet update

--- src/components/snippet/index.ts
+++ src/components/snippet/index.ts
@@ -1,3 +1,5 @@
 export { default as Snippet } from './snippet.astro';
 export { default as CodeWrapper } from './code_wrapper.astro';
 export { default as Raw } from './raw.astro';
+export { default as Preview } from './preview.astro';
+export { default as Iframe } from './iframe.astro';
