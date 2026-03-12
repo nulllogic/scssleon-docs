55c8e0a6d80d460d106f0a43d96061a7505b05b7
Vladimir Lukyanov <vladimir@lukyanov.net>
2026-03-12 15:17:18 +0300

    v0.6.232 – <raw> removed

--- src/components/snippet/index.ts
+++ src/components/snippet/index.ts
@@ -1,5 +1,4 @@
 export { default as Snippet } from './snippet.astro';
 export { default as CodeWrapper } from './code_wrapper.astro';
-export { default as Raw } from './raw.astro';
 export { default as Preview } from './preview.astro';
 export { default as Iframe } from './iframe.astro';
