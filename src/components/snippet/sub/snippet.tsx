1ad1433101edcf0e16393017a79b96585b04be33
vladimirlukyanov <vladimir@lukyanov.net>
2026-08-05 02:47:11 +0300

    v1.2.48 – snippet update

--- src/components/snippet/sub/snippet.tsx
+++ src/components/snippet/sub/snippet.tsx
@@ -1,53 +1,63 @@
 // src/components/MyReactComponent.jsx
 
-import Button from './button.tsx';
+import Code from './code.tsx';
 import styles from './snippet.module.scss';
 import clsx from 'clsx';
 
-export default function Snippet({title}) {
-
+export default function Snippet({title, html, scss, js, code = null, single = false}) {
+  // console.log(title, html, scss, js, code, single);
+  const lines = code ? code.split(/\r\n|\r|\n/).length : null;
+  // console.log(single && lines && lines > 1);
   return (
     <div className={clsx(styles.snippet)}>
-      <div className={clsx(styles.top)}>
-        <div className="nav">
-          <div className="left">
-            <div className={clsx(styles.title)}>
-              {title}
+      {single && lines && lines > 1 && (
+        <div className={clsx(styles.top)}>
+          <div className="nav">
+            <div className="left">
+              <div className={clsx(styles.title)}>
+                {title}
+              </div>
+            </div>
+            <div className="right">
+              2
             </div>
           </div>
-          <div className="right">
-            2
-          </div>
         </div>
-      </div>
+      )}
+
       <div className="middle">
         <div className="preview">
 
         </div>
-      </div>
-      <div className="bottom">
-        <div className="nav">
-          <div className="left">
-            3
-          </div>
-          <div className="right">
-            4
-          </div>
-        </div>
-        <div className="tabs">
-          <div className="tab">
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-          </div>
+        <div className="code">
+          <Code code={code} lang={'scss'} />
         </div>
       </div>
+      {!single && (
+        <div className="bottom">
+          <div className="nav">
+            <div className="left">
+              3
+            </div>
+            <div className="right">
+              4
+            </div>
+          </div>
+          <div className="tabs">
+            <div className="tab">
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
