8a0656b3cd2f058640bb64ec5353cbca0a11735b
vladimirlukyanov <vladimir@lukyanov.net>
2026-08-06 18:25:22 +0300

    v1.2.65 – snippet update

---
+++ src/components/snippet/sub/index.ts
@@ -0,0 +1,4 @@
+export { default as Preview } from './preview.astro';
+export { default as Button } from './button.astro';
+export { default as Iframe } from './iframe.astro';
+export { default as Code } from './code.astro';
